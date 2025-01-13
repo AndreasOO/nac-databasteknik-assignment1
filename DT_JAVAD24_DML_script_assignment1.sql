a9c99b177c79567ff8d7599d67cbabd9e747e76c
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-01-13 13:29:31 +0100

    second dml script added

--- DT_JAVAD24_DML_script_assignment1.sql
+++ DT_JAVAD24_DML_script_assignment1.sql
@@ -2,29 +2,38 @@
 
 SELECT `cst`.`name` FROM `shop_db`.`customers` AS `cst`
 INNER JOIN `shop_db`.`orders` AS `ord` 
-ON `cst`.`id`=`ord`.`customer_id`
-INNER JOIN `shop_db`.`order_items` AS `ord_item`
-ON `ord`.`id` = `ord_item`.`order_id`
+			ON `cst`.`id`=`ord`.`customer_id`
+INNER JOIN `shop_db`.`order_items` AS `ord_item` 
+			ON `ord`.`id` = `ord_item`.`order_id`
 INNER JOIN `shop_db`.`shop_items` AS `shop_item`
-ON `ord_item`.`shop_item_id` = `shop_item`.`id`
+			ON `ord_item`.`shop_item_id` = `shop_item`.`id`
 INNER JOIN `shop_db`.`specifications` AS `spec`
-ON `shop_item`.`specification_id` = `spec`.`id`
+			ON `shop_item`.`specification_id` = `spec`.`id`
 INNER JOIN `shop_db`.`products`AS `prod`
-ON `shop_item`.`product_id` = `prod`.`id`
+			ON `shop_item`.`product_id` = `prod`.`id`
 INNER JOIN `shop_db`.`brands` AS `brand`
-ON `prod`.`brand_id`=`brand`.`id`
+			ON `prod`.`brand_id`=`brand`.`id`
 INNER JOIN `shop_db`.`product_types` AS `prod_type`
-ON `prod`.`product_type_id` = `prod_type`.`id`
+			ON `prod`.`product_type_id` = `prod_type`.`id`
 INNER JOIN `shop_db`.`products_categories` AS `prod_cat_junc`
-ON `prod`.`id` = `prod_cat_junc`.`product_id`
+			ON `prod`.`id` = `prod_cat_junc`.`product_id`
 INNER JOIN `shop_db`.`categories` AS `cat`
-ON `prod_cat_junc`.`category_id` = `cat`.`id`
-WHERE `prod_type`.`name` = 'Shoes' AND `cat`.`name` = 'Sandals' AND `spec`.`size` = 38 AND `brand`.`name` = 'Ecco'
+			ON `prod_cat_junc`.`category_id` = `cat`.`id`
+WHERE `prod_type`.`name` = 'Shoes' 
+		AND `cat`.`name` = 'Sandals' 
+        AND `spec`.`size` = 38 
+        AND `brand`.`name` = 'Ecco'
 GROUP BY `cst`.`id`;
 
 
 -- Lista antalet produkter per kategori. Listningen ska innehålla kategori-namn och antalet produkter.
+SELECT `cat`.`name`, COUNT(`prod_cat_junc`.`category_id`) AS `num_products`  FROM `shop_db`.`products_categories` AS `prod_cat_junc`
+INNER JOIN `shop_db`.`categories` as `cat`
+			ON `prod_cat_junc`.`category_id`=`cat`.`id`    
+GROUP BY `cat`.`id`;    
 
+
+        
 -- Skapa en kundlista med den totala summan pengar som varje kund har handlat för. Kundens för- och efternamn, samt det totala värdet som varje person har shoppats för, skall visas.
 
 
