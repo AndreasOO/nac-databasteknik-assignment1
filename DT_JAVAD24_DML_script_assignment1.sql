bbe2e4a13f199b425ed98a4efdb7c1e18f78c9e4
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-01-13 15:51:41 +0100

    sixth dml script added

--- DT_JAVAD24_DML_script_assignment1.sql
+++ DT_JAVAD24_DML_script_assignment1.sql
@@ -78,7 +78,19 @@ GROUP BY `prod`.`id`
 ORDER BY `total_sold_units_product` DESC
 LIMIT 5;           
 
--- Vilken månad hade du den största försäljningen? (det måste finnas data som anger försäljning för mer än en månad i databasen för att visa att frågan är korrekt formulerad)
 
+
+
+-- Vilken månad hade du den största försäljningen? (det måste finnas data som anger försäljning för mer än en månad i databasen för att visa att frågan är korrekt formulerad)
+SELECT YEAR(`ord`.`date`) AS `year`, MONTH(`ord`.`date`) AS `month`, SUM(`prod`.`price`) AS `total_sold_price_month` FROM `shop_db`.`orders` AS `ord`
+INNER JOIN `shop_db`.`order_items` AS `ord_item`
+			ON `ord`.`id`=`ord_item`.`order_id`
+INNER JOIN `shop_db`.`shop_items` AS `shop_item`
+			ON `ord_item`.`shop_item_id` = `shop_item`.`id`
+INNER JOIN `shop_db`.`products` AS `prod`
+			ON `shop_item`.`product_id` = `prod`.`id`
+GROUP BY `month`, `year`
+ORDER BY `total_sold_price_month` DESC
+LIMIT 1; 
 
 -- TEST
